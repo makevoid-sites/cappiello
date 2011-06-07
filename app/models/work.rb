0f02b8f51f8711d824d0565c539e8eae9f11e5d3
Francesco Canessa <makevoid@gmail.com>
2011-06-07 07:36:52 +0200

    minor changes on works

--- app/models/work.rb
+++ app/models/work.rb
@@ -2,7 +2,7 @@ class Work
   include DataMapper::Resource
   
   property :id, Serial
-  property :name, String
+  property :name, String, length: 255
   property :created_at, DateTime
   property :text, Text 
   
