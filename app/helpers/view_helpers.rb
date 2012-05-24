b3d23c1dc2427aa3ef37ed7dc9152568ce3c11d6
makevoid <makevoid@gmail.com>
2012-05-24 22:50:21 +0200

    corrected page titles

--- app/helpers/view_helpers.rb
+++ app/helpers/view_helpers.rb
@@ -1,27 +1,27 @@
 module ViewHelpers
-  
+
   include NavHelper
-  
-  
+
+
   # basic texts
-  
+
   def site_title(content=nil)
     # old: Accademia d'Arte e Design Leonetto Cappiello
-    "Accademia Cappiello #{content} - Firenze"
+    "#{content}Accademia Cappiello"
   end
-  
+
   def footer_text
     raw "&copy;#{Time.now.year} Accademia Cappiello - Firenze"
   end
-  
+
   # SEO (and RDF)
-  
+
   def analytics_tracker
     "UA-20626831-1"
   end
-  
+
   def meta_author
     "Francesco 'makevoid' Canessa"
   end
-  
+
 end
