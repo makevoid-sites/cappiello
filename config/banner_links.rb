90cc705ea7414f54c1e4215e342dc5e8c5776f3c
Niccolò Pandolfini <nicco@filarete.net>
2012-10-08 21:42:52 +0200

    modifica immagini e conf banner

--- config/banner_links.rb
+++ config/banner_links.rb
@@ -1,24 +1,27 @@
 # nome file immagine => id della pagina
 {
+<<<<<<< HEAD
   "1"   => 1,
   "web_design"   => 45,
   "2"   => nil,
   "grafica_editoriale"   => 30,
   "3"   => nil,
+=======
+  "9"   => nil,
+  "webdesign"   => 45,
+  "2"   => nil,
+  "grficaeditoriale"   => 30,
+  "11"  => nil,
+>>>>>>> modifica immagini e conf banner
   "illustrazione"   => 43,
   "4"   => nil,
   "autocad"   => 49,
   "5"   => nil,
   "photoshop"   => 44,
-  "6"   => nil,
+  "12"  => nil,
   "3dstudiomax"   => 50,
   "7"   => nil,
   "sketchup"   => 48,
-  "8"   => nil,
-  "vray"   => 13,
-  "9"   => nil,
-  "10"  => nil,
-  "11"  => nil,
-  "12"  => nil,
   "13"  => nil,
+  "vray"   => 13,
 }
