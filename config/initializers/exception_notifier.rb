5c022bd3024701c72971f735ca1aa4794ff5be44
Francesco Canessa <makevoid@gmail.com>
2012-10-16 23:50:23 +0900

    Ripristinato link pdf, aggiunto @npandolfini alle notifiche errori

--- config/initializers/exception_notifier.rb
+++ config/initializers/exception_notifier.rb
@@ -3,7 +3,7 @@ if Rails.env == "production"
   Cappiello::Application.config.middleware.use ExceptionNotifier,
       :email_prefix => "[Cappiello] ",
       :sender_address => %{"Cappiello" <m4kevoid@gmail.com>},
-      :exception_recipients => %w{makevoid@gmail.com},
+      :exception_recipients => %w{makevoid@gmail.com nicco@filarete.net},
       ignore_exceptions: [ActionView::MissingTemplate]
 end
 
