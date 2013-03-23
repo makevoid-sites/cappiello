65e55c79963d12f23a46b4ce4dc1e68412a86244
Niccolò Pandolfini <nicco@filarete.net>
2013-03-23 13:37:05 +0100

    nuovi banner

--- config/banner_links.rb
+++ config/banner_links.rb
@@ -1,5 +1,6 @@
 # nome file immagine => id della pagina
 {
+  "visual_frame"   => 55,
   "9"   => nil,
   "web_design"   => 45,
   "2"   => nil,
@@ -7,13 +8,13 @@
   "11"  => nil,
   "illustrazione"   => 43,
   "4"   => nil,
-  "autocad"   => 49,
+  "pittura_creativa"   => 54,
   "5"   => nil,
-  "photoshop"   => 44,
+  "vray"   => 13,
   "12"  => nil,
   "3d_studio_max"   => 50,
   "7"   => nil,
   "sketchup"   => 48,
   "13"  => nil,
-  "vray"   => 13,
+  
 }
