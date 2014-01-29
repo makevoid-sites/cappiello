b3bb41c63047a71960d863a96bbd75b84334732d
Francesco Canessa <makevoid@gmail.com>
2014-01-29 19:03:42 +0100

    update map pin

--- public/javascripts/gmap.js
+++ public/javascripts/gmap.js
@@ -1,7 +1,7 @@
 $(function(){
   function gmap() {
     var mapDiv = document.getElementById('map_canvas')
-    var latLng = new google.maps.LatLng(43.76389, 11.269234)
+    var latLng = new google.maps.LatLng(43.7615854, 11.2725678)
     var map = new google.maps.Map(mapDiv, {
       center: latLng,
       zoom: 15,
