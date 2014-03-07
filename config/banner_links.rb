1a70a185409e5f2732737e9231ad8955ecbd1d11
Niccolò Pandolfini <nicco@filarete.net>
2014-03-07 11:42:57 +0100

    banner change II

--- config/banner_links.rb
+++ config/banner_links.rb
@@ -1,15 +1,13 @@
 # nome file immagine => id della pagina
 {
   "9"   => nil,
+  "2"   => nil,
   "visual_frame"   => 55,
   "12"  => nil,
   "illuminotecnica"   => 56,
   "13"  => nil,
   "web_design"   => 45,
-  "2"   => nil,
-  "grafica_editoriale"   => 30,
   "11"  => nil,
-  "illustrazione"   => 43,
   "4"   => nil,
   "pittura_creativa"   => 54,
   "5"   => nil,
