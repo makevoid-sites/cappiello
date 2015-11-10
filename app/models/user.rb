6cae4f23bed04782c6ddad700a2b10374ac50154
nicco77 <nicco@filarete.net>
2015-11-10 17:56:01 +0100

    interesse disegno/pittura

--- app/models/user.rb
+++ app/models/user.rb
@@ -50,6 +50,7 @@ class User
   property :int_stage_web,   Boolean, default: false
   property :int_stage_evening,   Boolean, default: false
   property :int_workshop_moda,   Boolean, default: false
+  property :int_disegno_pittura,   Boolean, default: false
   property :notes, Text
 
   property :qualification, String, length: 255
