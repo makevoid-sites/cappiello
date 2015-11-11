35289222a526152e6c345d745a134e92848d88d3
nicco77 <nicco@filarete.net>
2015-11-11 13:17:14 +0100

    nuovi banner header

--- config/banner_links.rb
+++ config/banner_links.rb
@@ -1,18 +1,17 @@
 # nome file immagine => id della pagina
 {
   "9"   => nil,
-  "becreative"   => 6,
-  "2"   => nil,
   "15mesi_interiordesign"   => 18,
   "12"  => nil,
   "15mesi_graficapubblicitaria_comunicazione"   => 16,
-  "13"  => nil,
+  "16"  => nil,
   "digitalcommunication"   => 53,
-  "11"  => nil,
+  "15"  => nil,
   "master_interiordesign"   => 40,
-  "4"   => nil,
+  "14"   => nil,
   "master_integratedmedia"   => 39,
-  "5"   => nil,
+  "16"   => nil,
+  "becreative"   => 6,
   "7"   => nil,
-  
+  "17"   => nil,  
 }
